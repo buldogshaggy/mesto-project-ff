7a855b770f0addf11922f1db475cb322d3fdf072
buldogshaggy <buldo9shaggy@yandex.ru>
2024-11-28 20:18:58 +0700

    Ready project 1.1

--- scripts/index.js
+++ scripts/index.js
@@ -1,47 +1,43 @@
-// @todo: Темплейт карточки
-// @todo: DOM узлы
+const container = document.getElementById('card-container');
 
+//Темплейт карточки
 function getTemplate() {
     return document.getElementById('card-template');
-}
+};
 
-const cards = [
-    {link: './images/ruba.jpg', name: 'Рубцовск', },
-    {link: './images/yarovoe.jpg', name: 'Яровое', },
-    ...initialCards,
-]
-
-// @todo: Вывести карточки на страницу
+//Выводим карточки
 function prepareCards() {
-    const cardTemplate = document.querySelector('#card-template');
-    console.log(cardTemplate);
+    const cardTemplate = getTemplate();
 
-    cards.forEach((x) => {
-        addCard(x.name, x.link)
-    })
-
-}
+    initialCards.forEach((x) => {
+        const cardElement = createCard(x.name, x.link);
+        container.append(cardElement);
+    });
+};
 
 prepareCards();
 
-// @todo: Функция создания карточки
-function addCard(name, link) {
-    const template = document.getElementById('card-template');
+//Создаем карточки
+function createCard(name, link, deleteCard) {
+    const template = getTemplate();
 
+    //Клонируем шаблон карточки
     const clone = template.content.cloneNode(true);
 
+    //Устанавливаем значение вложенных элементов
     const image = clone.querySelector('.card__image');
     image.src = link;
+    image.alt = name;
 
     const titleElement = clone.querySelector('.card__title');
     titleElement.textContent = name;
-  
-    const container = document.getElementById('card-container');
-    container.appendChild(clone);
-}
 
-// @todo: Функция удаления карточки
-function deleteCard(ctx) {
-    // @todo: опасно пиздец (передалть на id'шники, но они уже есть в template)
-    ctx.parentNode.remove()
-}
+    //Обработчикк клика по иконке удаления с колбеком
+    const deleteButton = clone.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', function() {
+        const listItem = deleteButton.closest('.card');
+        listItem.remove();
+    });
+
+    return clone;
+};
